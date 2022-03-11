d26d9d8d206ab90569055779c58e1047d815b84a
abresh1992 <abresh3hailemichael@gmail.com>
2022-03-11 01:52:05 -0800

    massage

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - entry point
  *
- * Return: Always 0 (success)
+ * Return: 0 after printing the function
  */
-
-int main(void) 
-{ 
+int main(void)
+{
 	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
