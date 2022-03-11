e533635194015fc9a26915ecd1878e9fbcd3c095
abresh1992 <abresh3hailemichael@gmail.com>
2022-03-11 02:27:28 -0800

    abr

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,6 +9,7 @@
 
 int main(void)
 {
+
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
@@ -20,4 +21,5 @@ int main(void)
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
 
 	return (0);
+
 }
