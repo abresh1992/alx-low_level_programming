177d813387a968f6cd185a0cf5bae1d8139b96a3
abresh1992 <abresh3hailemichael@gmail.com>
2022-03-12 03:00:57 -0800

    ab

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0(success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		putchar(a);
+	}
+
+	for (a = 97; a <= 102; a++)
+	{
+		putchar(a);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
