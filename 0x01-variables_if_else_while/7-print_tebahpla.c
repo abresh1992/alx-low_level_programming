9ad9f287c4b3332fbefc0b46a3e8bd0c924dc2bb
abresh1992 <abresh3hailemichael@gmail.com>
2022-03-12 02:52:01 -0800

    abrsh

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0(success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a == 122; a >= 97; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
+
