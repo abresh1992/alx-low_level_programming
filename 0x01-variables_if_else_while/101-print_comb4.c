6a7099e4acc64e8245d6bb93358383384c5f6eea
abresh1992 <abresh3hailemichael@gmail.com>
2022-03-12 04:11:38 -0800

    push

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,15 +6,14 @@
  */
 int main(void)
 {
-	int i,j,k,l;
-	
+	int i, j, k, l;
 	for (i = 0; i < 1000; i++)
 	{
 		j = i / 100;
 		k = (i / 10) % 10;
 		l = i % 10;
 
-		if (j < k && k<1)
+		if (j < k && k < 1)
 		{
 			putchar(j + '0');
 			putchar(k + '0');
