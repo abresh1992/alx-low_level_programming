c208613f9904b1c4ecea94f8560b3e1a487afbb9
abresh1992 <abresh3hailemichael@gmail.com>
2022-03-12 02:31:14 -0800

    message

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry ppoint
+ *
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 1o; n++)
+	{
+		printf("%n", n);
+	}
+	printf("\n");
+	
+	return (0);
+}
